7258d1f88e741235304ea830e36ef6d4ae4a7787
Ryan McCampbell <rmccampbell7@gmail.com>
2016-11-14 13:07:46 -0500

    CSV parsing

--- src/main/Parser.java
+++ src/main/Parser.java
@@ -1,5 +1,33 @@
 package main;
 
-public class Parser {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
+public class Parser {
+	public static ArrayList<Recipe> parseRecipeCSV(String file) {
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(new File(file));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		ArrayList<Recipe> recipes = new ArrayList<>();
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			List<String> tokens = Arrays.asList(line.split(","));
+			int id = Integer.valueOf(tokens.get(0));
+			String cuisine = tokens.get(1).replace("\"", "");
+			ArrayList<String> ingredients = new ArrayList<>(tokens.subList(2, tokens.size()));
+			for (int i=0; i < ingredients.size(); i++) {
+				ingredients.set(i, ingredients.get(i).replace("\"", ""));
+			}
+			recipes.add(new Recipe(id, cuisine, ingredients));
+		}
+		return recipes;
+	}
 }
---
+++ src/main/Recipe.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Recipe {
+	public int id;
+	public String cuisine;
+	public ArrayList<String> ingredients;
+
+	public Recipe(int id, String cuisine, ArrayList<String> ingredients) {
+		this.id = id;
+		this.cuisine = cuisine;
+		this.ingredients = ingredients;
+	}
+}
