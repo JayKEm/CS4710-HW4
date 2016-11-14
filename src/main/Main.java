c48748817ac2c1f7d8e865bd7414dea35d603005
Julian <jaykmcclinton@gmail.com>
2016-11-14 12:23:10 -0500

    Orgainizing

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+	
+	/*
+	 * Authors: Julian McClinton jm2af
+	 * 			Ryan McCampbell  rnm6u
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
